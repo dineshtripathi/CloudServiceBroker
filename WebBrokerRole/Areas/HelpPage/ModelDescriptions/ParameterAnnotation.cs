a4c04770aa907298330092c3747f1552fe5c74d3
Dinesh Tripathi <dinesh.tripathi@openenergi.com>
2016-09-06 14:01:35 +0100

    Add project files.

---
+++ WebBrokerRole/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebBrokerRole.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
